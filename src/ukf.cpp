06b6edd7a3ecac9064917a7a46bd51ad606e3987
adalberto-g <alebx32@gmail.com>
2017-07-14 13:41:14 -0500

    add SigmaPointPrediction initial

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -169,14 +169,14 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
 
   You'll also need to calculate the lidar NIS.
   */
-    MatrixXd Xsig_aug(n_aug_, n_aug_sigma_);
-    AugmentedSigmaPoints(x_, P_, Xsig_aug);
-    sigmapoints prediction
+    //MatrixXd Xsig_aug(n_aug_, n_aug_sigma_);
+    AugmentedSigmaPoints();
+    SigmaPointPrediction();
     predict mean and cov
 }
 
 
-void UKF::AugmentedSigmaPoints(MatrixXd* Xsig_out) {
+void UKF::AugmentedSigmaPoints() {
     
     //set state dimension
     //int n_x = 7;
@@ -226,8 +226,56 @@ void UKF::AugmentedSigmaPoints(MatrixXd* Xsig_out) {
         Xsig_aug.col(i+1+n_aug) = x_aug - sqrt(lambda+n_aug) * L.col(i);
     }
    
+}
 
-
+void UKF::SigmaPointPrediction() {
+    
+    //predict sigma points
+    for (int i = 0; i< n_sigma_points; i++)
+    {
+        //extract values for better readability
+        double p_x = Xsig_aug(0,i);
+        double p_y = Xsig_aug(1,i);
+        double v = Xsig_aug(2,i);
+        double yaw = Xsig_aug(3,i);
+        double yawd = Xsig_aug(4,i);
+        double nu_a = Xsig_aug(5,i);
+        double nu_yawdd = Xsig_aug(6,i);
+        
+        //predicted state values
+        double px_p, py_p;
+        
+        //avoid division by zero
+        if (fabs(yawd) > 0.001) {
+            px_p = p_x + v/yawd * ( sin (yaw + yawd*delta_t) - sin(yaw));
+            py_p = p_y + v/yawd * ( cos(yaw) - cos(yaw+yawd*delta_t) );
+        }
+        else {
+            px_p = p_x + v*delta_t*cos(yaw);
+            py_p = p_y + v*delta_t*sin(yaw);
+        }
+        
+        double v_p = v;
+        double yaw_p = yaw + yawd*delta_t;
+        double yawd_p = yawd;
+        
+        //add noise
+        px_p = px_p + 0.5*nu_a*delta_t*delta_t * cos(yaw);
+        py_p = py_p + 0.5*nu_a*delta_t*delta_t * sin(yaw);
+        v_p = v_p + nu_a*delta_t;
+        
+        yaw_p = yaw_p + 0.5*nu_yawdd*delta_t*delta_t;
+        yawd_p = yawd_p + nu_yawdd*delta_t;
+        
+        //write predicted sigma point into right column
+        Xsig_pred(0,i) = px_p;
+        Xsig_pred(1,i) = py_p;
+        Xsig_pred(2,i) = v_p;
+        Xsig_pred(3,i) = yaw_p;
+        Xsig_pred(4,i) = yawd_p;
+    }
+    
+    
 }
 
 /**
