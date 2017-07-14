6d45aab85c44efb4a5816fb07b3c3ee8086036fc
adalberto-g <alebx32@gmail.com>
2017-07-14 13:27:05 -0500

    add AugmentedSigmaPoints base

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -169,6 +169,65 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
 
   You'll also need to calculate the lidar NIS.
   */
+    MatrixXd Xsig_aug(n_aug_, n_aug_sigma_);
+    AugmentedSigmaPoints(x_, P_, Xsig_aug);
+    sigmapoints prediction
+    predict mean and cov
+}
+
+
+void UKF::AugmentedSigmaPoints(MatrixXd* Xsig_out) {
+    
+    //set state dimension
+    //int n_x = 7;
+    
+    //set augmented dimension
+    int n_aug = 7;
+    
+    //Process noise standard deviation longitudinal acceleration in m/s^2
+    double std_a = 0.2;
+    
+    //Process noise standard deviation yaw acceleration in rad/s^2
+    double std_yawdd = 0.2;
+    
+    //define spreading parameter
+    double lambda = 3 - n_aug;
+    
+    //create augmented mean vector
+    VectorXd x_aug = VectorXd(n_aug);
+    
+    //create augmented state covariance
+    MatrixXd P_aug = MatrixXd(7, 7);
+    
+    //create sigma point matrix
+    MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1);
+    
+    
+    //create augmented mean state
+    x_aug.head(5)=x;
+    x_aug(5)=0;
+    x_aug(6)=0;
+  
+    //create augmented covariance matrix
+    P_aug.fill(0.0);
+    P_aug.topLeftCorner(5,5) = P;
+    P_aug(5,5)= std_a * std_a;
+    P_aug(6,6)=std_yawdd * std_yawdd;
+    
+    
+    //create square root matrix
+    MatrixXd L = P_aug.llt().matrixL();
+    
+    //create augmented sigma points
+    Xsig_aug.col(0) = x_aug;
+    for (int i = 0; i < n_aug; i++)
+    {
+        Xsig_aug.col(i+1)     = x_aug + sqrt(lambda+n_aug) * L.col(i);
+        Xsig_aug.col(i+1+n_aug) = x_aug - sqrt(lambda+n_aug) * L.col(i);
+    }
+   
+
+
 }
 
 /**
